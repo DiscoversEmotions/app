9ec9efe910429f9f63ad215a20ce3423af406e4d
Etienne Dldc <etienne.dldc@outlook.fr>
2016-11-02 17:24:44 +0100

    Fix

--- src/client/component/WebGLMain/index.vue
+++ src/client/component/WebGLMain/index.vue
@@ -28,12 +28,12 @@ export default  {
 </script>
 
 <style scoped lang="scss">
-  .container {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: black;
-  }
+.container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: black;
+}
 </style>
