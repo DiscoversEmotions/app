b8564580f2534a82f6d3cfcf76ef6e4ae018c0ed
Etienne Dldc <etienne.dldc@outlook.fr>
2016-11-29 17:34:49 +0100

    Minor fix

--- src/client/webgl/MemoryWorld/MemoryWorld.js
+++ src/client/webgl/MemoryWorld/MemoryWorld.js
@@ -34,6 +34,7 @@ export class MemoryWorld {
   }
 
   mount() {
+    console.log(`mount Memory`);
     this.memorySound.setBuffer(this.app.assetsManager.getAsset(`memory1`));
     this.memorySound.play();
     this.memorySound.loop = true;
@@ -57,7 +58,11 @@ export class MemoryWorld {
     this.cube1.rotation.x += 0.01;
     this.cube1.rotation.y += 0.02;
 
-    this.material_cube1.emissive.b = this.analyser1.getAverageFrequency() / 256;
+    const audioLvl = this.analyser1.getAverageFrequency() / 256;
+
+    this.material_cube1.emissive.b = audioLvl;
+    const scale = 1 + audioLvl;
+    this.cube1.scale.set(scale, scale, scale);
   }
 
   setSize(width, height) {
