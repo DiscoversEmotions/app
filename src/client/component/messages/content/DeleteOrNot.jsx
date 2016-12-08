553fcb3e387b3a37940cbe809d7146d1d93ee674
Etienne Dldc <etienne.dldc@outlook.fr>
2016-12-08 13:38:30 +0100

    WIP UI

--- src/client/component/messages/content/DeleteOrNot.jsx
+++ src/client/component/messages/content/DeleteOrNot.jsx
@@ -3,13 +3,16 @@ import styled from 'styled-components';
 import _ from 'lodash';
 import Message from '~/component/messages/Message';
 import Button from '~/component/Button';
+import ErrorIcon from '~/component/icons/ErrorIcon';
 
 const DeleteOrNot = (props) => {
   const { msg } = props;
   return (
     <Message
       msg={msg}
+      type='error'
       title='Delete ?'
+      icon={ <ErrorIcon color='white' size={26} strokeWidth={35} /> }
     >
       Delete ?
       <Button>Press [enter] to delete</Button>
