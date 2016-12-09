2bba2ef7f099942dd4a23a3b07a8ddc7138abc93
Etienne Dldc <etienne.dldc@outlook.fr>
2016-12-09 14:28:13 +0100

    Creditrs

--- src/client/component/Credits.jsx
+++ src/client/component/Credits.jsx
@@ -5,7 +5,6 @@ import { compose, ConnectReact } from '~/core';
 import { inject } from 'react-tunnel';
 import LogoGobelins from '~/component/LogoGobelins';
 
-
 const enterAnim = keyframes`
   from {
     transform: translateY(-100%);
@@ -40,7 +39,7 @@ const Question  = styled.h3`
 
 const LeftContainer = styled.div`
   position: absolute;
-  top: 40%;
+  top: 50%;
   left: 15%;
   font-size: 16px;
   width: 40%;
@@ -48,7 +47,7 @@ const LeftContainer = styled.div`
 
 const RightContainer = styled.div`
   position: absolute;
-  top: 40%;
+  top: 50%;
   left: 65%;
   width: 25%;
   font-size: 16px;
@@ -67,62 +66,80 @@ const MainDescBloc = styled.p`
 const MainDescBlocContainer = styled.div`
   top: 25px;
   position: relative;
-`
+`;
 
 const TitleBloc = styled.h1`
   font-size: 18px;
   letter-spacing: 0.2em;
-`
+`;
 
 const DescBloc = styled.p`
   font-size: 16px;
-`
+`;
 
 const ListBloc = styled.ul`
   padding: 0;
-`
+`;
 
 const ListBlocElement = styled.li`
   list-style: none;
   padding-top: 5px;
-`
+`;
 
 const LeftItem = styled.div`
   margin-bottom: 30px;
-`
+`;
 
 const MentionsList = styled.ul`
   list-style: none;
   display: inline-flex;
-`
+`;
 
-const MentionsListElement = styled.li`
-`
+const Share = styled.a`
+  color: white;
+  font-weight: 100;
+  background: none;
+  border: none;
+  border-bottom: 1px solid white;
+  padding: 0.5em 0em;
+  margin-top: 1em;
+  font-size: 22px;
+  cursor: pointer;
+  transition-duration: .3s;
+  vertical-align: middle;
+  font-family: 'Anonymous Pro', monospace;
+  text-decoration: none;
 
-const GobelinsListElement = styled.li`
+  &:hover {
+    background: white;
+    color: black;
+  }
+`;
 
-`
+const MentionsListElement = styled.li``;
+
+const GobelinsListElement = styled.li``;
 
 const Credits = (props) => {
   return (
     <Container full={props.full || false }>
-      <Question>If you could erase some of your memories,<br />would you dare?</Question>
+      <Question>If you could erase some of your memories,<br />would you dare?<br /><Share target='_blank' href='https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fprojets.gobelins.fr%2Fdmii%2F2018%2Fremember%2F&amp;src=sdkpreparse'>Share your answer on Facebook</Share></Question>
       <LeftContainer>
         <MainTitleBloc>
           REMEMBER PROJET. GOBELINS, L’ECOLE DE L’IMAGE
         </MainTitleBloc>
         <MainDescBlocContainer>
           <MainDescBloc>
-            REMEMBER est une expérience interactive visant à nous provoquer et nous interroger sur nos propres choix, à travers un univers empli de mystère.
+            REMEMBER is as interactive experiment aiming to provoke and question us about our own choices.
           </MainDescBloc>
           <MainDescBloc>
-            Le réel enjeu de ce projet a été de nous faire comprendre le lien entre émotions et souvenirs; comprendre la façon unique dont ils sont connectés dans l'aventure d'un protagoniste qui part à la reconquête de ses émotions perdues.
+            Our project's main goal have been to make us understand the link between emotions and memories; and figure out the unique way that they are interconnected through the adventure of a protagonist that goes to reconquer his own lost emotions in a mysterious world, collecting little by little pieces from his past.
           </MainDescBloc>
           <MainDescBloc>
-            Cette expérience a été réalisée en webGL afin de rendre l'expérience la plus immersive possible.
+            That experiment has been made in webGL to make the experiment the most immersive possible.
           </MainDescBloc>
           <MainDescBloc>
-            Ce projet est l'aboutissement d'un travail de 2 mois, produit dans le cadre d'un projet pédagogique réalisé à Gobelins, l'École de l'Image.
+            This project is the result of 2 months worth of work, produced as a school project at Gobelins, l'École de l'Image
           </MainDescBloc>
         </MainDescBlocContainer>
       </LeftContainer>
